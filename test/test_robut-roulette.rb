936a52421394d34de858045e12814699a797766a
Richard Lynskey <richard@mozor.net>
2013-12-21 12:51:41 -0600

    Add mocha tests

--- test/test_robut-roulette.rb
+++ test/test_robut-roulette.rb
@@ -1,7 +1,43 @@
-require 'helper'
+require 'test_helper'
+require 'robut'
+require 'robut-roulette'
+require 'mocha/setup'
 
-class TestRobutRoulette < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class Robut::Plugin::RouletteTest < Test::Unit::TestCase
+
+  def setup
+    @connection = Robut::ConnectionMock.new
+    @presence = Robut::PresenceMock.new(@connection)
+    @plugin = Robut::Plugin::Roulette.new(@presence)
   end
+
+  def test_handle_roulette_shot_1_win
+    @plugin.stubs(:shot).returns(1)
+    @plugin.stubs(:draw).returns(0)
+    @plugin.handle(Time.now, "John", "roulette")
+    assert_equal( ["Shot 1 of 6: *click*"], @plugin.reply_to.replies )
+  end
+
+  # This one actually depends on the previous one to work
+  def test_handle_roulette_shot_2_repeat
+    @plugin.stubs(:shot).returns(1)
+    @plugin.stubs(:draw).returns(0)
+    @plugin.handle(Time.now, "John", "roulette")
+    assert_equal( ["John: you can't shoot twice in a row, dolt!"], @plugin.reply_to.replies )
+  end
+
+  def test_handle_roulette_shot_2_win
+    @plugin.stubs(:shot).returns(2)
+    @plugin.stubs(:draw).returns(0)
+    @plugin.handle(Time.now, "Jane", "roulette")
+    assert_equal( ["Shot 2 of 6: *click*"], @plugin.reply_to.replies )
+  end
+
+  def test_handle_roulette_shot_1_lose
+    @plugin.stubs(:shot).returns(1)
+    @plugin.stubs(:draw).returns(1)
+    @plugin.handle(Time.now, "Tim", "roulette")
+    assert_equal( ["Shot 1 of 6: (boom)", "*reloads*"], @plugin.reply_to.replies )
+  end
+
 end
